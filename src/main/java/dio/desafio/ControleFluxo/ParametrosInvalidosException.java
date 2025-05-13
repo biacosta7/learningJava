7645fa199f337403c8b81a8231fe44a9f1224ec3
biacosta7 <contact.beatrizcosta@gmail.com>
2025-05-13 11:16:02 -0300

    controle fluxo

---
+++ src/main/java/dio/desafio/ControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package dio.desafio.ControleFluxo;
+
+public class ParametrosInvalidosException extends Exception{
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
